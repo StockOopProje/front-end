de10de34db07f57ecf699bedb2a3b0f27d72ada3
OmBayus <omerbayramcavus@gmail.com>
2022-01-04 18:18:08 +0300

    zreport service

--- src/services/zreport.js
+++ src/services/zreport.js
@@ -2,8 +2,18 @@ import axios from "axios"
 
 const url = process.env.REACT_APP_API_URL+"zreport/" 
 
+const displayZReportDaily = async (date) =>{
+    return axios.post(url+"displayZReportDaily",date)
+}
 
+const displayZReportMonthly = async (date)=>{
+    return axios.post(url+"displayZReportMonthly",date)
+}
 
-const service = {}
+const displayZReportByDates = async (start,finish)=>{
+    return axios.post(url+"displayZReportByDates",{start,finish})
+}
+
+const service = {displayZReportDaily,displayZReportMonthly,displayZReportByDates}
 
 export default service
